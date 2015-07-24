9e5d64757e8000f29c5c3f419148861608f10746
Lam Truong An <an.lt@sutrixmedia.com>
2015-07-24 17:03:31 +0700

    sdfsF

--- src/StudentBundle/Controller/SubjectController.php
+++ src/StudentBundle/Controller/SubjectController.php
@@ -24,7 +24,6 @@ class SubjectController extends Controller
     public function indexAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
-        echo "test";
         $entities = $em->getRepository('StudentBundle:Subject')->findAll();
         $paginator  = $this->get('knp_paginator');
             $pagination = $paginator->paginate(
